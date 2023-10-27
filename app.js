020bc5fddea9eed21a9959945e50914c224819d0
ohnas <ohnas12@gmail.com>
2023-10-27 15:40:33 +0900

    init

---
+++ app.js
@@ -0,0 +1,61 @@
+const notiBtn = document.getElementById("noti-button");
+const saveButton = document.getElementById("saveButton");
+
+function notiInterval() {
+    // 1초 간격으로 console.log
+    let seconds = 1;
+    let logTimer = setInterval(() => {
+        console.log(`${seconds}초`);
+        seconds++;
+    }, 1000);
+
+    // 5초 간격으로 메시지를 보여줌
+    let notiTimer = setInterval(() => {
+        const notification = new Notification("Hello world");
+    }, 5000);
+
+    // 20초 후에 정지
+    setTimeout(() => {
+        clearInterval(logTimer);
+        clearInterval(notiTimer); 
+        console.log("Stop noti"); 
+    }, 20000);
+}
+
+function handleNotiBtn() {
+    if(Notification.permission === "granted") {
+        notiInterval();
+    } else if (Notification.permission !== "denied") {
+        Notification.requestPermission(function (permission) {
+            if (permission === "granted") {
+                notiInterval();
+            }
+        });
+    }
+}
+
+function byteChecking() {
+    const totalByte = 5242880;
+    const texts = localStorage.getItem('texts')
+    const encoder = new TextEncoder();
+    const uint8Array = encoder.encode(texts);
+    const percent = (uint8Array.byteLength / totalByte) * 100;
+    console.log(`currentByte : ${uint8Array.byteLength} Byte // totalByte : ${totalByte.toLocaleString("ko-KR")} Byte`);
+    console.log(`현재 사용량 : ${percent} %`)
+}
+
+function saveText(text) {
+    let texts = JSON.parse(localStorage.getItem('texts') || '[]');
+    texts.push(text);
+    localStorage.setItem('texts', JSON.stringify(texts));
+}
+
+notiBtn.addEventListener("click", handleNotiBtn);
+saveButton.addEventListener("click", function() {
+    const text = inputSentence.value;
+    if (text) {
+        saveText(text);
+        byteChecking();
+        inputSentence.value = "";
+    }
+});
---
+++ web2/scripts.js
@@ -0,0 +1,48 @@
+const inputSentence = document.getElementById("inputSentence");
+const saveButton = document.getElementById("saveButton");
+const savedTextList = document.getElementById("savedTextList");
+
+// 페이지 로드 시 저장된 문장 목록을 로드
+loadSavedTexts();
+
+function saveText(text) {
+    // 현재 저장된 문장들을 가져옴
+    let texts = JSON.parse(localStorage.getItem('texts') || '[]');
+    texts.push(text);
+    localStorage.setItem('texts', JSON.stringify(texts));
+}
+
+function loadSavedTexts() {
+    let texts = JSON.parse(localStorage.getItem('texts') || '[]');
+    texts.forEach(text => {
+        displayText(text);
+    });
+}
+
+function deleteText(event) {
+    const curretTextBox = event.target.parentElement;
+    curretTextBox.remove();
+}
+
+function displayText(text) {
+    const div = document.createElement("div");
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    div.classList.add("textBox");
+    li.textContent = text;
+    btn.textContent = "❌";
+    btn.classList.add("deleteBtn");
+    btn.addEventListener("click", deleteText);
+    div.appendChild(li);
+    div.appendChild(btn);
+    savedTextList.appendChild(div);
+}
+  
+saveButton.addEventListener("click", function() {
+    const text = inputSentence.value;
+    if (text) {
+        saveText(text);
+        displayText(text);
+        inputSentence.value = "";
+    }
+});
