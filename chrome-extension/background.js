b675f496a31c15a195409c8f9e525046fa4a9223
ohnas <ohnas12@gmail.com>
2023-11-15 20:21:26 +0900

    chrome-extension / manifiest.json

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,15 +1,15 @@
-// chrome.alarms.onAlarm.addListener(
-//     () => {
-//         chrome.notifications.create(
-//             {
-//                 type: "basic",
-//                 iconUrl: "hello_extensions.png",
-//                 title: "Hello",
-//                 message: "This is test",
-//                 silent: false
-//             },
-//             () => {console.log("테스트 알림 전송했습니다");}
-//         )
-//     }
-// )
+chrome.alarms.onAlarm.addListener(
+    () => {
+        chrome.notifications.create(
+            {
+                type: "basic",
+                iconUrl: "hello_extensions.png",
+                title: "Hello",
+                message: "This is test",
+                silent: false
+            },
+            () => {console.log("테스트 알림 전송했습니다");}
+        )
+    }
+)
 
