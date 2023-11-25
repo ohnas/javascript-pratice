e94b34a5fe3984f6b741007e11bc830cee85a83a
ohnas <ohnas12@gmail.com>
2023-11-26 00:33:44 +0900

    chrome_extension_new // modify background logic

--- chrome_extension_new/background.js
+++ chrome_extension_new/background.js
@@ -1,31 +1,36 @@
 chrome.runtime.onMessage.addListener(
-    function (request) {
-        texts = request.notiTexts;
+    function(request) {
+        const texts = request.notiTexts;
+        const times = request.alarmTimes;
         console.log('onmessage', texts);
-        times = request.alarmTimes;
         console.log('onmessage', times);
-        if(texts.length !== 0 && times.length !== 0) {
-            times.forEach((time, index) => {
-                chrome.alarms.create(`testAlarm_${index}`, {
-                    when: Date.now() + time
-                });
-                chrome.alarms.onAlarm.addListener(
-                    () => {
-                        const randomIndex = Math.floor(Math.random() * texts.length);
-                        const randomElement = texts[randomIndex];
-                        chrome.notifications.create(
-                            {
-                                type: "basic",
-                                iconUrl: "hello_extensions.png",
-                                title: "Hello",
-                                message: randomElement,
-                                silent: false
-                            },
-                            () => {console.log("알림 전송 되었습니다.");}
-                        )
-                    }
-                )
-            });
-        }
+        createAlarm(times, texts);
     }
 );
+
+chrome.alarms.onAlarm.addListener(
+    function(alarm) {
+        chrome.notifications.create(
+            {
+                type: "basic",
+                iconUrl: "hello_extensions.png",
+                title: "Hello",
+                message: alarm.name,
+                silent: false
+            },
+            () => { console.log("알림 전송 되었습니다."); }
+        );
+    }
+);
+
+function createAlarm(times, texts) {
+    if (texts.length !== 0 && times.length !== 0) {
+        times.forEach((time) => {
+            let randomIndex = Math.floor(Math.random() * texts.length);
+            let randomElement = texts[randomIndex];
+            chrome.alarms.create(`${randomElement}`, {
+                when: Date.now() + time
+            });
+        });
+    }
+}
